1ad7193c140973b72a6096ef8350e92eeeedb96d
Daniel Ledezma <dledezma@me.com>
2020-06-21 13:37:23 -0500

    facebook authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '602606987024395',
+        clientSecret: '0c8b610566ddd020bee753febfbb5b9d'
+    }
 }
